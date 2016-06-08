a0b329affc43c59c952b2f1798abc566c842f267
千锋 <qianfeng@Timoduizhangzhengzaidaiming.local>
2016-06-08 14:24:39 +0800

    创建爱限免工程

---
+++ FXFreeLimit/FXFreeLimit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FXFreeLimit
+//
+//  Created by 千锋 on 16/6/8.
+//  Copyright © 2016年 FengXiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FXFreeLimit/FXFreeLimit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FXFreeLimit
+//
+//  Created by 千锋 on 16/6/8.
+//  Copyright © 2016年 FengXiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
