697e7d96e662489a590c6ac7c6f69daad83009ee
千锋 <qianfeng@Timoduizhangzhengzaidaiming.local>
2016-06-08 14:48:44 +0800

    测试：加了一个视图控制器,并且设置了背景颜色

---
+++ FXFreeLimit/视图控制器/FXViewController.h
@@ -0,0 +1,13 @@
+//
+//  FXViewController.h
+//  FXFreeLimit
+//
+//  Created by 千锋 on 16/6/8.
+//  Copyright © 2016年 FengXiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FXViewController : UIViewController
+
+@end
